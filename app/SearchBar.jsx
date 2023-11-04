1c27a7e9baf0b772d3e964a88a08189ef9be777a
Mason Watamura <2003xcrun@gmail.com>
2023-11-04 01:12:54 +0100

    search bar working

--- app/SearchBar.jsx
+++ app/SearchBar.jsx
@@ -3,32 +3,32 @@ import "./SeachBar.css"
 
 export default function SearchBar() {
     const [filterProducts, setFilteredProducts] = useState([])
-    const astyle = {width:"150px"}
+    const [searchString, setSearchString] = useState("")
+    useEffect(fetchAPIData, [searchString])
 
     function fetchAPIData(){ 
         fetch('https://fakestoreapi.com/products?limit=20')
       .then(res=>res.json())
       .then(products=>{
-        const currentFilter = products.map((product, index) => {
+        const filtered = products.filter((list)=>{return list.title.toLowerCase().includes(searchString.toLowerCase())})
+        const currentFilter = filtered.map((product, index) => {
             return (
-                <div class="prodStyle">
+                <div class="prodStyle" key={index}>
                     <p>{product.title}</p>
-                    <p>{product.price}</p>
-                    <img style={astyle} src={product.image} alt={product.title} />
-                </div>
-            )
+                    <p>${product.price}</p>
+                    <img style={{width:"100px"}} src={product.image} alt={product.title} />
+                </div>)
         })
         setFilteredProducts(currentFilter)
       })
     }
 
-    function filter (product) {
-        return product.includes()
+    function handleChange(event) {
+        setSearchString(event.target.value)
     }
-
-    useEffect(fetchAPIData, [])
+    
     return (<div>
-        <input type="text"/>
+        <input type="text" value={searchString} onChange={handleChange}/>
         <div style={{display:"flex", flexFlow:"row wrap"}}>
             {filterProducts}
         </div>
